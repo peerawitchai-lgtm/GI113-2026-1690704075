fa5097c5fc42c223f1a0b51f822d01275c988c25
peerawitchai-lgtm <peerawit.chai@bumail.net>
2026-08-25 16:33:26 +0700

    idk
    idk

---
+++ Lab02/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab02
+{
+    internal class Program
+    {
+        static void Main(string[] args) //สมโป้ง
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
