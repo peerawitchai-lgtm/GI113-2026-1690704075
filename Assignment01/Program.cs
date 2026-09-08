343bf1d56403135311b9c5a4b2af306e3f5e5a3e
peerawitchai-lgtm <peerawit.chai@bumail.net>
2026-09-08 16:37:25 +0700

    assign01ver1.0
    ยังไม่เสร็จ

---
+++ Assignment01/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Student ID :1690704075
+ * Name       :Assignment01
+ * Section    :129D
+ * No.        :N/A
+ * Course     : GI113 Computer Programming (GI)
+ */namespace Assignment01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string gameName = "test"; //const
+            var charName = "John Kaisen";
+            var charRank = 'S';
+            int charLevel = 100;
+            double charHealth = 100.0;
+            float charCrit = 0.5f;
+            bool charIsPlayable = true;
+
+        }
+    }
+}
